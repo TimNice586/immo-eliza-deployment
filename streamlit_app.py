054fd3674fa53e66a08b5a89fe1500f8e027d746
Tim De Nijs <timdenijsketels@gmail.com>
2025-12-05 15:27:51 +0100

    added a terrace yes no appear only on yes field

--- streamlit_app.py
+++ streamlit_app.py
@@ -154,20 +154,29 @@ if property_type != correct_type:
     property_type = correct_type
 
 st.subheader("Property details")
-# Other fields
+
+# Basic inputs
 living_area = st.number_input("Living Area (m²)", min_value=25, max_value=400000)
 bedrooms = st.number_input("Bedrooms", min_value=0, max_value=50)
-terrace_area = st.number_input("Terrace Area (m²)", min_value=0, max_value=150)
-equiped_kitchen = st.checkbox("Equipped Kitchen")
-swimming_pool = st.checkbox("Swimming Pool")
-open_fire = st.checkbox("Open Fire")
+
+# Optional features with conditional inputs
 terrace = st.checkbox("Terrace")
+terrace_area = st.number_input("Terrace Area (m²)", min_value=0, max_value=150) if terrace else None
+
 garden = st.checkbox("Garden")
+garden_area = st.number_input("Garden Area (m²)", min_value=0, max_value=300) if garden else None
+
+swimming_pool = st.checkbox("Swimming Pool")  # no area needed, just boolean
+equiped_kitchen = st.checkbox("Equipped Kitchen")
+open_fire = st.checkbox("Open Fire")
 furnished = st.checkbox("Furnished")
-state_of_building = st.selectbox("State", options=['Excellent','To be renovated',
-        'New','Normal','Fully renovated',
-        'To renovate','Under construction','To demolish',
-         'To restore'])
+
+state_of_building = st.selectbox(
+    "State",
+    options=['Excellent','To be renovated','New','Normal','Fully renovated',
+             'To renovate','Under construction','To demolish','To restore']
+)
+
 facades = st.selectbox("Facades", options=[1, 2, 3, 4])
 
 required_cols = {
@@ -186,22 +195,24 @@ def ensure_all_features(input_dict):
 
 if st.button("Predict Price"):
     input_data = {
-        "living_area (m²)": int(living_area),
-        "number_of_bedrooms": int(bedrooms),
-        "terrace_area (m²)": int(terrace_area),
-        "equiped_kitchen (yes:1, no:0)": int(equiped_kitchen),
-        "swimming_pool (yes:1, no:0)": int(swimming_pool),
-        "terrace (yes:1, no:0)": int(terrace),
-        "region": region,
-        "province": province,
-        "number_facades": facades,
-        "open_fire (yes:1, no:0)": open_fire,
-        "type": property_type,
-        "subtype": subtype,
-        "garden (yes:1, no:0)": garden,
-        "furnished (yes:1, no:0)": furnished,
-        "state_of_building": state_of_building 
-    }
+    "living_area (m²)": int(living_area),
+    "number_of_bedrooms": int(bedrooms),
+    "terrace_area (m²)": terrace_area,
+    "equiped_kitchen (yes:1, no:0)": int(equiped_kitchen),
+    "swimming_pool (yes:1, no:0)": int(swimming_pool),
+    "terrace (yes:1, no:0)": int(terrace),
+    "region": region,
+    "province": province,
+    "number_facades": facades,
+    "open_fire (yes:1, no:0)": int(open_fire),
+    "type": property_type,
+    "subtype": subtype,
+    "garden (yes:1, no:0)": int(garden),
+    "furnished (yes:1, no:0)": int(furnished),
+    "state_of_building": state_of_building,
+    "postal_code": postal_code
+}
+
     
     input_data = ensure_all_features(input_data)
     price = predict(input_data)
